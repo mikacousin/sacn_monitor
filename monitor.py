2bc73e65cb25a750f7c0dab0a11f8a0d04bc0dbb
mikacousin <mika.cousin@gmail.com>
2020-11-17 11:17:49 +0100

    Optimization: only redraw changed level outputs

--- monitor.py
+++ monitor.py
@@ -31,13 +31,21 @@ def callback(packet):
     Args:
         packet (sacn.DataPacket): DMX data
     """
-    for output, level in enumerate(packet.dmxData):
+    univ = packet.universe
+    dmx_data = list(packet.dmxData)
+    # Create a list with only changed level outputs to not redraw all universe
+    diff = [
+        (index, e1)
+        for index, (e1, e2) in enumerate(zip(dmx_data, old_dmx_data[univ]))
+        if e1 != e2
+    ]
+    for output, level in diff:
         try:
-            univ = packet.universe
             App().win.outputs[univ, output].level = level
             GLib.idle_add(App().win.outputs[univ, output].queue_draw)
         except AttributeError:
             pass
+    old_dmx_data[univ] = dmx_data
 
 
 class Window(Gtk.ApplicationWindow):
@@ -93,15 +101,18 @@ def run_monitor():
     app_monitor = Application()
     receiver = sacn.sACNreceiver()
     receiver.start()
-    for universe in UNIVERSES:
-        receiver.join_multicast(universe)
-        receiver.register_listener("universe", callback, universe=universe)
+    for univ in UNIVERSES:
+        receiver.join_multicast(univ)
+        receiver.register_listener("universe", callback, universe=univ)
     app_monitor.run(sys.argv)
     receiver.stop()
 
 
 # sACN monitored universes (1-63999)
 UNIVERSES = [1, 2, 4]
+old_dmx_data = {}
+for universe in UNIVERSES:
+    old_dmx_data[universe] = [0] * 512
 
 run_profile = os.environ.get("SACN_MONITOR_PROFILING", False)
 
